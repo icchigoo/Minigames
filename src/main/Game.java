3147c9e94a48e87faf2bb57bf787f82f2e34fbd6
Aawash cheetry <79775763+icchigoo@users.noreply.github.com>
2022-11-08 13:13:40 +0545

    complete

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,13 +1,17 @@
 package main;
 
 import java.awt.Graphics;
+
+import audio.AudioPlayer;
+import gamestates.Credits;
+import gamestates.GameOptions;
 import gamestates.Gamestate;
 import gamestates.Menu;
 import gamestates.Playing;
+import ui.AudioOptions;
 
 public class Game implements Runnable {
 
-	private GameWindow gameWindow;
 	private GamePanel gamePanel;
 	private Thread gameThread;
 	private final int FPS_SET = 120;
@@ -15,6 +19,10 @@ public class Game implements Runnable {
 
 	private Playing playing;
 	private Menu menu;
+	private Credits credits;
+	private GameOptions gameOptions;
+	private AudioOptions audioOptions;
+	private AudioPlayer audioPlayer;
 
 	public final static int TILES_DEFAULT_SIZE = 32;
 	public final static float SCALE = 2f;
@@ -24,20 +32,24 @@ public class Game implements Runnable {
 	public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
 	public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
 
+	private final boolean SHOW_FPS_UPS = true;
+
 	public Game() {
+		System.out.println("size: " + GAME_WIDTH + " : " + GAME_HEIGHT);
 		initClasses();
-
 		gamePanel = new GamePanel(this);
-		gameWindow = new GameWindow(gamePanel);
-		gamePanel.requestFocus();
-
+		new GameWindow(gamePanel);
+		gamePanel.requestFocusInWindow();
 		startGameLoop();
-
 	}
 
 	private void initClasses() {
+		audioOptions = new AudioOptions(this);
+		audioPlayer = new AudioPlayer();
 		menu = new Menu(this);
 		playing = new Playing(this);
+		credits = new Credits(this);
+		gameOptions = new GameOptions(this);
 	}
 
 	private void startGameLoop() {
@@ -47,37 +59,26 @@ public class Game implements Runnable {
 
 	public void update() {
 		switch (Gamestate.state) {
-		case MENU:
-			menu.update();
-			break;
-		case PLAYING:
-			playing.update();
-			break;
-		case OPTIONS:
-		case QUIT:
-		default:
-			System.exit(0);
-			break;
-
+		case MENU -> menu.update();
+		case PLAYING -> playing.update();
+		case OPTIONS -> gameOptions.update();
+		case CREDITS -> credits.update();
+		case QUIT -> System.exit(0);
 		}
 	}
 
+	@SuppressWarnings("incomplete-switch")
 	public void render(Graphics g) {
 		switch (Gamestate.state) {
-		case MENU:
-			menu.draw(g);
-			break;
-		case PLAYING:
-			playing.draw(g);
-			break;
-		default:
-			break;
+		case MENU -> menu.draw(g);
+		case PLAYING -> playing.draw(g);
+		case OPTIONS -> gameOptions.draw(g);
+		case CREDITS -> credits.draw(g);
 		}
 	}
 
 	@Override
 	public void run() {
-
 		double timePerFrame = 1000000000.0 / FPS_SET;
 		double timePerUpdate = 1000000000.0 / UPS_SET;
 
@@ -91,6 +92,7 @@ public class Game implements Runnable {
 		double deltaF = 0;
 
 		while (true) {
+
 			long currentTime = System.nanoTime();
 
 			deltaU += (currentTime - previousTime) / timePerUpdate;
@@ -98,26 +100,32 @@ public class Game implements Runnable {
 			previousTime = currentTime;
 
 			if (deltaU >= 1) {
+
 				update();
 				updates++;
 				deltaU--;
+
 			}
 
 			if (deltaF >= 1) {
+
 				gamePanel.repaint();
 				frames++;
 				deltaF--;
-			}
-
-			if (System.currentTimeMillis() - lastCheck >= 1000) {
-				lastCheck = System.currentTimeMillis();
-				System.out.println("FPS: " + frames + " | UPS: " + updates);
-				frames = 0;
-				updates = 0;
 
 			}
+
+			if (SHOW_FPS_UPS)
+				if (System.currentTimeMillis() - lastCheck >= 1000) {
+
+					lastCheck = System.currentTimeMillis();
+					System.out.println("FPS: " + frames + " | UPS: " + updates);
+					frames = 0;
+					updates = 0;
+
+				}
+
 		}
-
 	}
 
 	public void windowFocusLost() {
@@ -131,5 +139,21 @@ public class Game implements Runnable {
 
 	public Playing getPlaying() {
 		return playing;
+	}
+
+	public Credits getCredits() {
+		return credits;
+	}
+
+	public GameOptions getGameOptions() {
+		return gameOptions;
+	}
+
+	public AudioOptions getAudioOptions() {
+		return audioOptions;
+	}
+
+	public AudioPlayer getAudioPlayer() {
+		return audioPlayer;
 	}
 }
